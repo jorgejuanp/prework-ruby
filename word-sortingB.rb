fbf2db085028c10bb104f8df3ce0b1bb13a54f0c
Jorge Juan <hi.jorgejuanp@gmail.com>
2015-09-25 11:59:44 +0200

    Add exercise Word sorting

---
+++ word-sortingB.rb
@@ -0,0 +1,19 @@
+
+src = gets.chomp
+
+def stnc_to_array(sentence)
+	words_array = sentence.split(" ") #split the sentence into words
+	words_array.sort #sort the words
+	
+	#remove punctuation characters
+	for item in words_array
+		item.gsub(/[^A-Za-z0-9\s]/i, '')
+	end
+	
+	# sort using a custom function that ignores cases when comparing words 
+	# (you can compare both strings after calling upcase on them), but keeping 
+	# the capitalized words as they were in the beginning .
+	words_array.sort {|a, b| a.upcase <=> b.upcase}
+end
+
+puts stnc_to_array(src)
